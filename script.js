b875ca5a7f18885834fb62eb853cc66ae838dfc5
Marcus Vinicius <102832530+Blue02z@users.noreply.github.com>
2024-01-12 18:32:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+function accordionOpen(){
+    const accordionContent = document.querySelector(".accordion-content")
+    accordionContent.classList.toggle("open")
+    const indicadorAberto = document.getElementById("indicador-aberto")
+    const indicadorFechado = document.getElementById("indicador-fechado")
+    console.log("erro")
+    if(accordionContent.classList.contains("open")){
+        indicadorAberto.style = "display:flex;"
+        indicadorFechado.style = "display:none;"
+    }else{
+        indicadorAberto.style = "display:none;"
+        indicadorFechado.style = "display:flex;"
+    }
+}
+
+function accordionOpen2(){
+    const accordionContent = document.querySelector(".accordion-content2")
+    accordionContent.classList.toggle("open")
+    const indicadorAberto = document.getElementById("indicador-aberto2")
+    const indicadorFechado = document.getElementById("indicador-fechado2")
+    console.log("erro")
+    if(accordionContent.classList.contains("open")){
+        indicadorAberto.style = "display:flex;"
+        indicadorFechado.style = "display:none;"
+    }else{
+        indicadorAberto.style = "display:none;"
+        indicadorFechado.style = "display:flex;"
+    }
+}
+
+
+function accordionOpen3(){
+    const accordionContent = document.querySelector(".accordion-content3")
+    accordionContent.classList.toggle("open")
+    const indicadorAberto = document.getElementById("indicador-aberto3")
+    const indicadorFechado = document.getElementById("indicador-fechado3")
+    console.log("erro")
+    if(accordionContent.classList.contains("open")){
+        indicadorAberto.style = "display:flex;"
+        indicadorFechado.style = "display:none;"
+    }else{
+        indicadorAberto.style = "display:none;"
+        indicadorFechado.style = "display:flex;"
+    }
+}
+
+function accordionOpen4(){
+    const accordionContent = document.querySelector(".accordion-content4")
+    accordionContent.classList.toggle("open")
+    const indicadorAberto = document.getElementById("indicador-aberto4")
+    const indicadorFechado = document.getElementById("indicador-fechado4")
+    console.log("erro")
+    if(accordionContent.classList.contains("open")){
+        indicadorAberto.style = "display:flex;"
+        indicadorFechado.style = "display:none;"
+    }else{
+        indicadorAberto.style = "display:none;"
+        indicadorFechado.style = "display:flex;"
+    }
+}
+
+
+function accordionOpen5(){
+    const accordionContent = document.querySelector(".accordion-content5")
+    accordionContent.classList.toggle("open")
+    const indicadorAberto = document.getElementById("indicador-aberto5")
+    const indicadorFechado = document.getElementById("indicador-fechado5")
+    console.log("erro")
+    if(accordionContent.classList.contains("open")){
+        indicadorAberto.style = "display:flex;"
+        indicadorFechado.style = "display:none;"
+    }else{
+        indicadorAberto.style = "display:none;"
+        indicadorFechado.style = "display:flex;"
+    }
+}
